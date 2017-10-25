ca6a267164ded9434d7dca9cd31116ba11a1ed17
Alexandria Lim <Limria2011@gmail.com>
2017-10-24 21:15:51 -1000

    Add files via upload

---
+++ SelectionSorter.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SelectionSorter {
+	public static void selectionSort(ArrayList<String> wordList) {
+		Collections.sort(wordList);
+		
+	}
+}
